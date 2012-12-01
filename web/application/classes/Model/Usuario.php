efe3a251eebc5ba93ffcbc12d879c6844493354e
Gerardo Villarroel <gerardo@localhost.localdomain>
2012-12-01 20:39:54 -0300

    fix url kohana

--- web/application/classes/Model/Usuario.php
+++ web/application/classes/Model/Usuario.php
@@ -1,6 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Model_Usuario extends ORM {
+    
+    protected $_table_name = "Usuario";
     //put your code here
     public $id;
     public $nombre;
