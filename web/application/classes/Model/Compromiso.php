d691757790b5773c5a55db928b2bf8135970cfa4
cesar2012 <ceanbumo@gmail.com>
2013-01-02 16:53:42 -0300

    El nombre de la tabla es con mayusculas

--- web/application/classes/Model/Compromiso.php
+++ web/application/classes/Model/Compromiso.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
  
 class Model_Compromiso extends ORM {
-     protected $_table_name = "Compromiso";
+     protected $_table_name = "COMPROMISO";
 	 protected $_primary_key = 'ID_COMPROMISO';
 }
