9c35522b1b39377e3a7c87b3512df8c2c266540b
cesar2012 <ceanbumo@gmail.com>
2013-01-02 16:34:53 -0300

    actualizacion view foro

--- web/application/views/foro/ini.php
+++ web/application/views/foro/ini.php
@@ -1,19 +1,17 @@
-<p>Instrucciones para el foro</p>
+
+
+<H2>Condiciones de uso del foro</H2>
+<br/>
+<table width=900><tr><td>Se solicita no enviar ningun contenido abusivo, vulgar, difamatorio. Recuerde no afectar los valores promovidos por "Emeres". Recuerde que "Emeres" tiene derecho a eliminar, editar, mover o cerrar cualquier tema en cualquier momento que lo creamos conveniente. Dado que esta información no será compartida con ninguna tercera parte sin su consentimiento, "Emeres" no podrán considerarse responsables por cualquier intento de hacking que conlleve a que los datos sean comprometidos.</td></tr></table>
 <br/>
 <br/>
-<form action="http://localhost/foro/ucp.php?mode=login" method="post">
-<input  type="hidden" name="username" size="25" value="<?php echo $user->nombre  ?>" tabindex="1" />  			
-<input type="hidden" name="password" size="25" value="uEmJqI0j5StY" tabindex="2" />
+<form name="formulario1" id="formulario1" action="http://localhost/foro/ucp.php?mode=login" method="post">
+<input  type="hidden" name="username" size="25" value="<?php echo $usuario->NICK  ?>"  />  			
+<input type="hidden" name="password" size="25" value="uEmJqI0j5StY"  />
 
 <input type="hidden" name="redirect" value="http://localhost/foro/viewforum.php?f=4&t=4" />
-<input type="submit" name="login" class="btnmain" value="Ingresar al Foro" tabindex="5" />
+<input type="submit" name="login" value="Ingresar al Foro"  />
 
 </form>
-<?php
-//echo "->".$usuario->nombre."<-";
-//echo Form::label("usuario", I18n::get("base.menu.bienvenido")) . $usuario->nombre;
-//echo $usuario;
 
-//echo "b: ".$user->nombre;
-//print_r(usuario);
-?>
+
